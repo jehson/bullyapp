e2edcd1e2d54b03b21fcb01ba89e592bc923126c
Jehson Kin <j.n.heos@gmail.com>
2013-10-23 19:17:13 +0800

    DELETED ALL

--- grails-app/conf/spring/resources.groovy
+++
@@ -1,8 +0,0 @@
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
-// Place your Spring DSL code here
-beans = {
-	commonsMultipartResolver(org.springframework.web.multipart.commons.CommonsMultipartResolver){
-		maxUploadSize = 1000000
-	}
-}
